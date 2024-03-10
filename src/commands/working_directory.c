b357f90b32d9cdeae595d417859047e28c89d151
Mezz <m.mazouz67@gmail.com>
2024-03-10 22:18:38 +0100

    drop the 🎤

--- src/commands/working_directory.c
+++ src/commands/working_directory.c
@@ -10,30 +10,13 @@
 #include <errno.h>
 #include <stdio.h>
 
-
-static char *get_absolute_path(char *relative_path)
-{
-    char *path;
-    char *root = server_get_root(NULL);
-
-    if (relative_path[0] == '/') {
-        path = malloc(strlen(root) + strlen(relative_path) + 1);
-        strcpy(path, server_get_root(NULL));
-        strcat(path, relative_path);
-    } else
-        path = relative_path;
-    return path;
-}
-
 void command_cwd(client_t *client, char **command)
 {
     char *path;
 
     if (command[1] == NULL || strcmp(command[1], "") == 0)
         return client_send(client, "501 No directory name specified.");
-    if (strncmp(command[1], "..", 2) == 0)
-        return command_pwd(client, command);
-    path = get_absolute_path(command[1]);
+    path = command[1];
     if (chdir(path) == -1)
         return client_send(client, _550);
     getcwd(client->current_path, 1024);
@@ -46,7 +29,7 @@ void command_pwd(client_t *client, __attribute__((unused)) char **command)
     char buffer[2048] = {0};
 
     getcwd(path, 1024);
-    sprintf(buffer , "257 \"%s\" created.", path);
+    sprintf(buffer, "257 \"%s\" created.", path);
     client_send(client, buffer);
 }
 
